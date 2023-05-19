aacf8abc15af3c9a8325772bc2ef4ee155df5206
Al Mamun Khan <almamunkhan09@gmail.com>
2023-05-19 23:54:47 +0200

    Import routes to show in the front page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+// Import the router-dom
+import { BrowserRouter } from 'react-router-dom';
+// Import the components
+import About from './components/About';
+import Experience from './components/Experience';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Tech from './components/Tech';
+
+// Create the App component
 function App() {
-  return <>3d portfolio</>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
